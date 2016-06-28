b76d215a584955cb43bfd33e25b8d6109778f4ff
Ravi Jonnadula <rjonnadu@linkedin.com>
2016-06-28 12:50:55 -0700

    - Integrate OpenBFDD into OPS including OVSDB integration
    Change-Id: I7b9718d418ab1171dba55244e6e1dc809973f7aa
    Signed-off-by: Ravi Jonnadula <rjonnadu@linkedin.com>

---
+++ CommandProcessor.h
@@ -0,0 +1,59 @@
+/**************************************************************
+* Copyright (c) 2010-2013, Dynamic Network Services, Inc.
+* Jake Montgomery (jmontgomery@dyn.com) & Tom Daly (tom@dyn.com)
+* Distributed under the FreeBSD License - see LICENSE
+***************************************************************/
+// Class for handling command communication with the control app.
+#pragma once
+
+#include "SockAddr.h"
+
+class Beacon;
+
+/**
+ * Interface factory. Use delete to free this.
+ *
+ * Requires UtilsInit() to have been called.
+ *
+ * @throw - May throw an exception.
+ *
+ * @return CommandProcessor* - Will not return NULL.
+ */
+class CommandProcessor* MakeCommandProcessor(Beacon &beacon);
+
+#ifdef ENABLE_OVSDB
+class Scheduler;
+class CommandProcessor* MakeOvsCommandProcessor(Scheduler *scheduler, Beacon *beacon);
+#endif
+
+/**
+ * This interface handles all communication between this beacon and the control
+ * utility. Most of the processing is done in a worker thread.
+ *
+ */
+class CommandProcessor
+{
+public:
+  virtual ~CommandProcessor() { };
+
+  /**
+   *
+   * Starts listening, and handling commands, on the given address and port. This
+   * will block until socket communication can be established. This should not be
+   * called from different threads simultaneously.
+   *
+   * @param addr [in] - The address and port on which to listen.
+   *
+   * @return bool - false if communication could not be set up.
+   */
+  virtual bool BeginListening(const SockAddr &addr) = 0;
+
+  /**
+   * Halts listening and waits for the listen thread to terminate.
+   * Do not call simultaneously with BeginListening().
+   */
+  virtual void StopListening() = 0;
+
+protected:
+  CommandProcessor(Beacon &) { };
+};
